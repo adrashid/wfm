10436b55f0421fec5cb63496731077e953cdaef8
Adnan Rashid, PhD <adnan.hvg@gmail.com>
2026-04-30 07:05:54 +0500

    Update weather_forecasting_SCR.pm

--- weather_forecasting_SCR.pm
+++ weather_forecasting_SCR.pm
@@ -7,62 +7,62 @@ dtmc
 
 // Transition probabilities
 
-const double P1 = 135/1000;
+const double P1 = 1293/10000;
 const double P2 = 1 - P1;
 
-const double P3 = 65/100;
-const double P4 = 25/100;
+const double P3 = 3382/10000;
+const double P4 = 5735/10000;
 const double P5 = 1 - P3 - P4;
 
-const double P6 = 65/100;
+const double P6 = 8214/10000;
 const double P7 = 1 - P6;
 
-const double P8 = 50/100;
-const double P9 = 25/100;
+const double P8 = 1905/10000;
+const double P9 = 7143/10000;
 const double P10 = 1 - (P8 + P9);
 
-const double P11 = 325/1000;
+const double P11 = 4286/10000;
 const double P12 = 1 - P11;
 
-const double P13 = 80/100;
+const double P13 = 9459/10000;
 const double P14 = 1 - P13;
 
-const double P15 = 40/100;
-const double P16 = 45/100;
+const double P15 = 7308/10000;
+const double P16 = 1923/10000;
 const double P17 = 1 - (P15 + P16);
 
-const double P18 = 20/100;
+const double P18 = 2833/10000;
 const double P19 = 1 - P18;
 
-const double P20 = 70/100;
-const double P21 = 20/100;
+const double P20 = 1536/10000;
+const double P21 = 1321/10000;
 const double P22 = 1 - (P20 + P21);
 
-const double P23 = 75/100;
+const double P23 = 6909/10000;
 const double P24 = 1 - P23;
 
-const double P25 = 65/100;
+const double P25 = 2308/10000;
 const double P26 = 1 - P25;
 
-const double P27 = 25/100;
-const double P28 = 50/100;
+const double P27 = 2157/10000;
+const double P28 = 5882/10000;
 const double P29 = 1 - (P27 + P28);
 
-const double P30 = 43/100;
+const double P30 = 1111/10000;
 const double P31 = 1 - P30;
 
-const double P32 = 40/100;
-const double P33 = 20/100;
+const double P32 = 6061/10000;
+const double P33 = 2121/10000;
 const double P34 = 1 - (P32 + P33);
 
-const double P35 = 60/100;
+const double P35 = 4706/10000;
 const double P36 = 1 - P35;
 
-const double P37 = 30/100;
-const double P38 = 60/100;
+const double P37 = 625/1000;
+const double P38 = 125/1000;
 const double P39 = 1 - (P37 + P38);
 
-const double P40 = 70/100;
+const double P40 = 75/100;
 const double P41 = 1 - P40;
 
 
