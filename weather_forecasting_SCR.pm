15a96489942b3a8ff112d60d0da00b5046d20b31
Adnan Rashid, PhD <adnan.hvg@gmail.com>
2026-04-30 08:07:00 +0500

    Update weather_forecasting_SCR.pm

--- weather_forecasting_SCR.pm
+++ weather_forecasting_SCR.pm
@@ -110,6 +110,24 @@ module TOWFM1
     [] x = 16 -> P37:(x' = 8) + P38:(x' = 7) + P39:(x' = 9);
 
 
+// Rain-dominant states (last day is rainy or strong rain persistence)
+label "rain" = (x=4 | x=9 | x=10 | x=11 | x=12 | x=13 | x=14);
+
+// Strong rain persistence (core rainy sequences)
+label "heavy_rain" = (x=10 | x=12);
+
+// Fully rainy sequence
+label "RRR" = (x=12);
+
+// Fully cloudy sequence
+label "CCC" = (x=8);
+
+// Fully sunny sequence
+label "SSS" = (x=0);
+
+// Dry weather (no rain)
+label "dry" = (x=0 | x=1 | x=2 | x=3 | x=5 | x=6 | x=7 | x=8);
+
 
 endmodule
 
