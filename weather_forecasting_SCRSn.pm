46462a75a87d3f4da4fea08e49b1bacef9f7818d
Adnan Rashid, PhD <adnan.hvg@gmail.com>
2026-04-30 07:13:04 +0500

    Update weather_forecasting_SCRSn.pm

--- weather_forecasting_SCRSn.pm
+++ weather_forecasting_SCRSn.pm
@@ -4,99 +4,99 @@
 
 dtmc
 
-const double P1 = 62/100;
+const double P1 = 428/10000;
 const double P2 = 1 - P1;
 
-const double P3 = 16/100;
-const double P4 = 64/100;
+const double P3 = 921/10000;
+const double P4 = 8158/10000;
 const double P5 = 1 - P3 - P4;
 
-const double P6 = 42/100;
+const double P6 = 9167/10000;
 const double P7 = 1 - P6;
 
-const double P8 = 65/100;
-const double P9 = 115/1000;
+const double P8 = 1127/10000;
+const double P9 = 8169/10000;
 const double P10 = 1 - (P8 + P9);
 
-const double P11 = 465/1000;
+const double P11 = 5/10;
 const double P12 = 345/1000;
 const double P44 = 1 - P11 - P12;
 
-const double P13 = 35/100;
+const double P13 = 8235/10000;
 const double P14 = 1 - P13;
 
-const double P15 = 42/100;
-const double P16 = 25/100;
+const double P15 = 5333/10000;
+const double P16 = 2667/10000;
 const double P17 = 1 - (P15 + P16);
 
-const double P18 = 65/100;
+const double P18 = 1556/10000;
 const double P19 = 1 - P18;
 
-const double P20 = 70/100;
-const double P21 = 15/100;
+const double P20 = 512/10000;
+const double P21 = 879/10000;
 const double P22 = 1 - (P20 + P21);
 
-const double P23 = 30/100;
-const double P24 = 35/100;
+const double P23 = 5792/10000;
+const double P24 = 325/10000;
 const double P42 = 1 - (P23 + P24);
 
 const double P25 = 45/100;
-const double P26 = 29/100;
+const double P26 = 25/100;
 const double P47 = 1 - (P25 + P26);
 
-const double P27 = 40/100;
-const double P28 = 37/100;
+const double P27 = 2792/10000;
+const double P28 = 7143/10000;
 const double P29 = 1 - (P27 + P28);
 
 
-const double P30 = 45/100;
-const double P31 = 30/100;
+const double P30 = 5410/10000;
+const double P31 = 25/100;
 const double P48 = 1 - (P30 + P31);
 
-const double P32 = 30/100;
-const double P33 = 49/100;
+const double P32 = 6143/10000;
+const double P33 = 2857/10000;
 const double P34 = 1 - (P32 + P33);
 
-const double P35 = 35/100;
-const double P36 = 30/100;
+const double P35 = 45/100;
+const double P36 = 25/100;
 const double P43 = 1 - (P35 + P36);
 
-const double P37 = 25/100;
-const double P38 = 60/100;
+const double P37 = 7971/10000;
+const double P38 = 145/10000;
 const double P39 = 1 - (P37 + P38);
 
-const double P40 = 47/100;
+const double P40 = 456/10000;
 const double P41 = 1 - P40;
 
-const double P45 = 55/100;
+const double P45 = 576/1000;
 const double P46 = 1 - P45;
 
-const double P50 = 50/100;
+const double P50 = 45/100;
 const double P51 = 1 - P50;
 
-const double P52 = 47/100;
+const double P52 = 455/1000;
 const double P53 = 1 - P52;
 
-const double P54 = 25/100;
-const double P55 = 35/100;
+const double P54 = 35/100;
+const double P55 = 40/100;
 const double P56 = 1 - (P54 + P55);
 
-const double P57 = 40/100;
+const double P57 = 325/1000;
 const double P58 = 1 - P57;
 
-const double P59 = 35/100;
-const double P60 = 30/100;
+const double P59 = 40/100;
+const double P60 = 345/1000;
 const double P61 = 1 - (P59 + P60);
 
-const double P62 = 45/100;
-const double P63 = 35/100;
+const double P62 = 42/100;
+const double P63 = 355/1000;
 const double P64 = 1 - (P62 + P63);
 
-const double P68 = 60/100;
+const double P68 = 568/1000;
 const double P65 = 1 - P68;
 
-const double P66 = 35/100;
-const double P67 = 25/100;
+const double P66 = 365/1000;
+const double P67 = 20/100;
 const double P49 = 1 - (P66 + P67);
 
 
