f1c38746ae10d890a3a68f7bd3815522a6936686
Adnan Rashid, PhD <adnan.hvg@gmail.com>
2026-06-18 12:32:51 +0500

    Update First_Order_SCRSn.pm

--- First_Order_SCRSn.pm
+++ First_Order_SCRSn.pm
@@ -13,19 +13,16 @@ const double Pss  = 23/50;
 const double Psc  = 27/50;
 
 // Cloudy
-const double Pcs  = 11/25;
-const double Pcc  = 17/50;
-const double Pcr  = 9/50;
-const double Pcsn = 3/50;
+const double Pcs  = 44/100;
+const double Pcc  = 34/100;
+const double Pcr  = 22/100;
 
 // Rainy
-const double Prs  = 7/50;
-const double Prc  = 29/100;
-const double Prr  = 21/50;
-const double Prsn = 9/100;
+const double Prc  = 31/100;
+const double Prr  = 58/100;
+const double Prsn = 11/100;
 
 // Snowy
-const double Psn_s  = 13/200;
 const double Psn_c  = 21/100;
 const double Psn_r  = 31/100;
 const double Psn_sn = 33/100;
@@ -35,7 +32,7 @@ const double Psn_sn = 33/100;
 // DTMC MODULE
 // ==============================
 
-module WFM1
+module WFMSCRSNO1
 
     x : [0..3] init 0;
 
@@ -44,16 +41,12 @@ module WFM1
     // 2 = R
     // 3 = Sn
 
-    [] x = 0 ->
-        Pss:(x'=0) + Psc:(x'=1);
+    [] x = 0 -> Pss:(x'=0) + Psc:(x'=1);
 
-    [] x = 1 ->
-        Pcs:(x'=0) + Pcc:(x'=1) + Pcr:(x'=2) + Pcsn:(x'=3);
+    [] x = 1 -> Pcs:(x'=0) + Pcc:(x'=1) + Pcr:(x'=2) + Pcsn:(x'=3);
 
-    [] x = 2 ->
-        Prs:(x'=0) + Prc:(x'=1) + Prr:(x'=2) + Prsn:(x'=3);
+    [] x = 2 -> Prs:(x'=0) + Prc:(x'=1) + Prr:(x'=2) + Prsn:(x'=3);
 
-    [] x = 3 ->
-        Psn_s:(x'=0) + Psn_c:(x'=1) + Psn_r:(x'=2) + Psn_sn:(x'=3);
+    [] x = 3 -> Psn_s:(x'=0) + Psn_c:(x'=1) + Psn_r:(x'=2) + Psn_sn:(x'=3);
 
 endmodule
